ff6ef8ac64873909eb24d760362d7358b3f6f95d
knockknockyoo <iheaven0129@gmail.com>
2025-07-22 17:10:38 -0400

    Added the case-insensitive search.

--- domain/mongo-pagination.js
+++ domain/mongo-pagination.js
@@ -1,17 +1,22 @@
 const {getSortDirection} = require("../utility/mongodb-utility");
 
 class MongoPagination {
-    constructor(first, offset, orderBy, sortDirection) {
+    constructor(first, offset, orderBy, sortDirection, isCaseInsensitive = false) {
         this.first = first;
         this.offset = offset;
         this.orderBy = orderBy;
         this.sortDirection = sortDirection;
+        this.isCaseInsensitive = isCaseInsensitive;
     }
 
     getPaginationPipeline() {
         const pipeline = [];
         if (this.orderBy) {
-            pipeline.push({"$sort": { [this.orderBy]: getSortDirection(this.sortDirection) } });
+            const orderBy = this.isCaseInsensitive ? `${this.orderBy}_lower` : this.orderBy;
+            if (this.isCaseInsensitive) {
+                pipeline.push({$addFields: {[orderBy]: { $toLower: `$${this.orderBy}` }}});
+            }
+            pipeline.push({"$sort": { [orderBy]: getSortDirection(this.sortDirection) } });
         }
 
         if (this.offset) {
