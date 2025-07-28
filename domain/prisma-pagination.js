c5c6c46a4fb87a2153409677a118a0aa4d076a7f
knockknockyoo <iheaven0129@gmail.com>
2025-07-28 14:13:18 -0400

    Init.

---
+++ domain/prisma-pagination.js
@@ -0,0 +1,39 @@
+const {SORT} = require("../../constants/db-constants");
+
+class PrismaPagination {
+    constructor(first, offset, orderBy, sortDirection) {
+        this._first = first;
+        this._offset = offset;
+        this._orderBy = orderBy;
+        this._sortDirection = sortDirection || SORT.DESC;
+    }
+
+    getPagination() {
+        let pagination = {};
+        if (this._orderBy) {
+            pagination[this._orderBy] = this._getSortDirection();
+        }
+
+        if (this._offset) {
+            pagination.skip = this._offset;
+        }
+
+        if (!(Number.isInteger(this?._first) && this?._first === -1)) {
+            pagination.take = this._first;
+        }
+
+        return pagination;
+    }
+
+    getNoLimit() {
+        return (this._orderBy) ? { [this._orderBy]: this._getSortDirection() } : {};
+    }
+
+    _getSortDirection() {
+        return this._sortDirection?.toLowerCase() === SORT.DESC ? SORT.DESC : SORT.ASC;
+    }
+}
+
+module.exports = {
+    PrismaPagination
+}
