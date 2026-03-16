b3d7797db7f7e0621243a4af42bcabee36268ebd
Alejandro-Vega <vegaalejandro15@gmail.com>
2026-03-16 14:24:11 -0400

    add line break in chatbot floating button label

--- src/components/ChatBot/context/ChatBotContext.tsx
+++ src/components/ChatBot/context/ChatBotContext.tsx
@@ -27,7 +27,7 @@ export type ChatBotProviderProps = {
 
 export const ChatBotProvider: React.FC<ChatBotProviderProps> = ({
   title = "Chat",
-  label = "CRDC Assistant",
+  label = "CRDC\nAssistant",
   knowledgeBaseUrl = "",
   children,
 }) => {
