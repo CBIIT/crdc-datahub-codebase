3c6e940b0c614650501c498d0827ff3b0983867e
Austin Mueller <64418315+AustinSMueller@users.noreply.github.com>
2026-04-13 13:49:43 -0400

    3578: constant for number of valid study statuses

--- constants/approved-study-constants.js
+++ constants/approved-study-constants.js
@@ -6,6 +6,10 @@ const APPROVED_STUDY_STATUS = Object.freeze({
     INACTIVE: "Inactive",
 });
 
+/** Max length of the `statuses` filter array for listApprovedStudies (one slot per distinct valid status). */
+const APPROVED_STUDY_STATUS_FILTER_MAX_LENGTH = Object.keys(APPROVED_STUDY_STATUS).length;
+
 module.exports = {
     APPROVED_STUDY_STATUS,
+    APPROVED_STUDY_STATUS_FILTER_MAX_LENGTH,
 };
