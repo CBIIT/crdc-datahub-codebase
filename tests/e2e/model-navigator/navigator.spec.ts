d911f400a92fa1b884b47037cf9027f32ee6229e
Alec M. <alec.mattu@nih.gov>
2026-08-14 11:02:53 -0400

    Separate model navigator test cases

--- tests/e2e/model-navigator/navigator.spec.ts
+++ tests/e2e/model-navigator/navigator.spec.ts
@@ -1,43 +1,40 @@
 import { test } from '../../fixtures';
 
 test.describe('Model Navigator', { tag: ["@unauthenticated"] }, () => {
-  test('model navigator base', async ({ modelNavigatorPage }) => {
-    await modelNavigatorPage.openGcModel();
+  test('should load the page successfully', async ({ modelNavigatorPage }) => {
+    await modelNavigatorPage.openModel("GC");
     await modelNavigatorPage.expectOnModelNavigator();
   });
 
-  test('file downloads', async ({ modelNavigatorPage }) => {
-    await modelNavigatorPage.openGcModel();
+  test('should download the TSV template file', async ({ modelNavigatorPage }) => {
+    await modelNavigatorPage.openModel("GC");
     await modelNavigatorPage.switchToTableView();
-
-    await modelNavigatorPage.page.screencast.showChapter('Template Download', {
-      description: 'Clicking the Template Download button to download the template file.',
-      duration: 2000,
-    });
 
     const templateDownloadButton = modelNavigatorPage.page
       .getByRole('button', { name: 'Template Download' })
       .first();
-    const templateDownloadPromise = modelNavigatorPage.page.waitForEvent('download');
+    const downloadPromise = modelNavigatorPage.page.waitForEvent('download');
     await templateDownloadButton.click();
-    const download = await templateDownloadPromise;
+    const download = await downloadPromise;
     test.info().annotations.push({ type: 'info', description: `Downloaded file named: ${download.suggestedFilename()}` });
     test.info().attach('template.tsv', { path: await download.path(), contentType: 'text/tab-separated-values' });
+  });
 
-    await modelNavigatorPage.page.screencast.showChapter('Data Dictionary', {
-      description: 'Clicking the Data Dictionary Download button to download the data dictionary file.',
-      duration: 2000,
-    });
+  test('should download the Data Dictionary PDF file', async ({ modelNavigatorPage }) => {
+    await modelNavigatorPage.openModel("GC");
+    await modelNavigatorPage.switchToTableView();
 
     const dataDictionaryDownloadButton = modelNavigatorPage.page
       .getByRole('button', { name: 'Data Dictionary Download' })
       .first();
+
     await dataDictionaryDownloadButton.click();
-    const download1Promise = modelNavigatorPage.page.waitForEvent('download');
+    const downloadPromise = modelNavigatorPage.page.waitForEvent('download');
     await modelNavigatorPage.page.getByRole('menuitem', { name: 'All Properties (PDF)' }).click();
-    const download1 = await download1Promise;
-    test.info().annotations.push({ type: 'info', description: `Downloaded file named: ${download1.suggestedFilename()}` });
-    test.info().attach('data-dictionary.pdf', { path: await download1.path(), contentType: 'application/pdf' });
+    const download = await downloadPromise;
+
+    test.info().annotations.push({ type: 'info', description: `Downloaded file named: ${download.suggestedFilename()}` });
+    test.info().attach('data-dictionary.pdf', { path: await download.path(), contentType: 'application/pdf' });
   });
 });
 
--- tests/pages/model-navigator.page.ts
+++ tests/pages/model-navigator.page.ts
@@ -4,27 +4,22 @@ import { BasePage } from './base.page';
 
 export class ModelNavigatorPage extends BasePage {
   readonly modelNavigatorButton: Locator;
-  readonly gcModelLink: Locator;
-  readonly categoryButton: Locator;
   readonly tableViewTab: Locator;
 
   constructor(page: Page) {
     super(page);
     this.modelNavigatorButton = page.getByRole('button', { name: 'Model Navigator' });
-    this.gcModelLink = page.getByRole('link', { name: 'GC Model' });
-    this.categoryButton = page.getByRole('button', { name: 'Category', exact: true });
     this.tableViewTab = page.getByRole('tab', { name: 'Table View' });
   }
 
-  async openGcModel(): Promise<void> {
+  async openModel(model: string): Promise<void> {
     await this.navigate('/');
     await this.modelNavigatorButton.click();
-    await this.gcModelLink.click();
+    await this.page.getByRole('link', { name: `${model} Model` }).click();
     await this.waitForUrlPath(/\/model-navigator\/.+\/latest(?:\?.*)?$/);
   }
 
   async switchToTableView(): Promise<void> {
-    await this.categoryButton.click();
     await this.tableViewTab.click();
   }
 
