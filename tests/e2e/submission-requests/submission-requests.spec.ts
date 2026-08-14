20a5ddc0815ea7f928304899e6eb5c63baa63e7d
Alec M. <alec.mattu@nih.gov>
2026-08-14 10:27:44 -0400

    fix broken create SRF flow

--- tests/e2e/submission-requests/submission-requests.spec.ts
+++ tests/e2e/submission-requests/submission-requests.spec.ts
@@ -1,8 +1,12 @@
 import { test } from '../../fixtures';
 
-test('should create a blank submission request form', async ({ page, submissionRequestsPage }) => {
-  await submissionRequestsPage.open();
-  
-  await submissionRequestsPage.createSRF();
-  await page.screenshot();
+test.describe('Submission Requests', () => {
+  test('should create a blank submission request form', async ({ page, submissionRequestsPage }) => {
+    await submissionRequestsPage.open();
+    await test.info().attach('SRF-Page.png', { body: await page.screenshot({ fullPage: true }), contentType: 'image/png' });
+    await submissionRequestsPage.openCreateSRFModal();
+    await test.info().attach('Create-SRF-Modal.png', { body: await page.screenshot({ fullPage: true }), contentType: 'image/png' });
+    await submissionRequestsPage.confirmCreateSRF();
+    await test.info().attach('New-SRF-Page.png', { body: await page.screenshot({ fullPage: true }), contentType: 'image/png' });
+  });
 });
--- tests/pages/submission-requests.page.ts
+++ tests/pages/submission-requests.page.ts
@@ -11,23 +11,27 @@ export class SubmissionRequestsPage extends BasePage {
     super(page);
     this.pageTitle = page.getByRole('heading', { name: 'Submission Requests' });
     this.createSrfButton = page.getByTestId('create-application-button');
-    this.createSrfConfirmButton = page.getByRole('button', { name: 'I Read and Accept' });
+    this.createSrfConfirmButton = page.getByRole("dialog").getByTestId('delete-dialog-confirm-button');
   }
 
   async open(): Promise<void> {
     await this.navigate('/submission-requests');
 
     await this.waitForUrlPath(/\/submission-requests(?:\?.*)?$/);
-    await expect(this.pageTitle).toBeVisible();
   }
 
-  async createSRF(): Promise<void> {
+  async openCreateSRFModal(): Promise<void> {
     await expect(this.createSrfButton).toBeVisible();
     await this.createSrfButton.click();
 
     await expect(this.createSrfConfirmButton).toBeVisible();
+  }
+
+  async confirmCreateSRF(): Promise<void> {
     await this.createSrfConfirmButton.click();
 
     await this.waitForUrlPath(/\/submission-request\/new(?:\?.*)?$/);
+
+    await expect(this.page.getByRole('heading', { name: 'Submission Request Form' })).toBeVisible();
   }
 }
