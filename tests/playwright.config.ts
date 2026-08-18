3b83b0c4d07f0daa936e2b5143eef59e07c2304c
Alec M. <alec.mattu@nih.gov>
2026-08-18 10:18:37 -0400

    Drop safari tests

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -55,23 +55,5 @@ export default defineConfig({
       grep: /@unauthenticated/,
       testIgnore: /.*\.setup\.ts/
     },
-    {
-      name: 'Safari (Desktop)',
-      use: {
-        ...devices['Desktop Safari'],
-        storageState: process.env.AUTH_STATE_PATH,
-      },
-      grepInvert: /@unauthenticated/,
-      dependencies: ['setup'],
-      testIgnore: /.*\.setup\.ts/
-    },
-    {
-      name: "Safari (Desktop)",
-      use: {
-        ...devices['Desktop Safari'],
-      },
-      grep: /@unauthenticated/,
-      testIgnore: /.*\.setup\.ts/
-    }
   ]
 });
