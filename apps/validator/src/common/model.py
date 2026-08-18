dab2f7d63dc8fc49699e34e7dbb870ec84d8a5d9
Ming Ying <yingming18@gmail.com>
2026-08-17 23:32:58 -0400

    Validated required relationships using bento-mdf

--- apps/validator/src/common/model.py
+++ apps/validator/src/common/model.py
@@ -52,6 +52,17 @@ class DataModel:
         if not props:
             return None
         return {k: v for (k, v) in props.items() if v.get("required") == True}
+
+    """
+    get required relationships of a node in the model
+    """
+    def get_node_req_rel_columns(self, node):
+        edges = self.mdf_model.edges.values() if self.mdf_model else []
+        req_rel_columns = []
+        for edge in edges:
+            if edge.src.handle == node and edge.is_required:
+                req_rel_columns.append(self.edge_to_column_name(edge))
+        return req_rel_columns
     
     """
     get file nodes in the model
@@ -112,8 +123,21 @@ class DataModel:
     def get_data_commons(self):
         return self.model.get("data_commons", None)
 
+    def edge_to_column_name(self, edge):
+        if edge is None:
+            return ''
+        node = edge.dst
+        key = self.get_node_key_prop(node.handle)
+        return f'{node.handle}.{key}'
 
-    
+    def get_node_key_prop(self, node):
+        props = self.mdf_model.nodes[node].props if self.mdf_model else {}
+        key = None
+        for prop in props.values():
+            if prop.is_key:
+                key = prop.handle
+                break
+        return key
 
     
     
