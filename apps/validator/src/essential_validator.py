6e066b6e13fcb6739b9af91c03d1c75d8ef0fa75
Ming Ying <yingming18@gmail.com>
2026-09-16 14:38:28 -0400

    Fixed miss matched parentheses

--- apps/validator/src/essential_validator.py
+++ apps/validator/src/essential_validator.py
@@ -508,7 +508,7 @@ class EssentialValidator:
             # check missing required proper 
             required_props = self.model.get_node_req_props(type)
             system_populated_props = self.model.get_system_populated_prop_list()
-            final_required_props = list(set(required_props -set(system_populated_props)))
+            final_required_props = list(set(required_props) -set(system_populated_props))
             missed_props = [ prop for prop in final_required_props if prop not in columns and prop != id_field]
             if len(missed_props) > 0:
                 msg = f'“{file_info[FILE_NAME]}”: '
